6b090a28ab4376b2d8e67d5c4e037ca374c98e0f
Jan <he.klocuszek123@gmail.com>
2026-03-22 16:08:54 +0100

    Rotating cube without projection without RPY

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{ thread, time };
+
 struct Point {
     x: f64,
     y: f64,
@@ -11,10 +13,8 @@ impl Point {
 
     fn rotate_x (&mut self, angle: f64) {
         let radians: f64 = angle.to_radians();
-        let x: f64 = self.x;
         let y: f64 = self.y * radians.cos() - self.z * radians.sin();
         let z: f64 = self.y * radians.sin() + self.z * radians.cos();
-        self.x = x;
         self.y = y;
         self.z = z;
     }
@@ -22,10 +22,8 @@ impl Point {
     fn rotate_y (&mut self, angle: f64) {
         let radians: f64 = angle.to_radians();
         let x: f64 = self.x * radians.cos() + self.z * radians.sin();
-        let y: f64 = self.y;
         let z: f64 = -1.0 * self.x * radians.sin() + self.z * radians.cos();
         self.x = x;
-        self.y = y;
         self.z = z;
     }
 
@@ -33,14 +31,26 @@ impl Point {
         let radians: f64 = angle.to_radians();
         let x: f64 = self.x * radians.cos() - self.y * radians.sin();
         let y: f64 = self.x * radians.sin() + self.y * radians.cos();
-        let z: f64 = self.z;
         self.x = x;
         self.y = y;
-        self.z = z;
     }
+
+    // This just spins one point at the time
+    // --------------------------------------
+    // fn spin_z (&mut self) {
+    //     let duration: time::Duration = time::Duration::from_millis(500);
+    //     loop {
+    //         for i in 1..=365 {
+    //             self.rotate_z(i as f64);
+    //             self.show();
+    //             thread::sleep(duration);
+    //         }
+    //     }
+    // }
 }
 
 fn main() {
+    let duration: time::Duration = time::Duration::from_millis(500);
     let p1: Point = Point {
         x: 0.5,
         y: 0.7,
@@ -82,11 +92,21 @@ fn main() {
         z: -0.5,
     };
 
-    let points: [Point; 8] = [p1, p2, p3, p4, p5, p6, p7, p8];
-    for mut p in points {
+    let mut points: [Point; 8] = [p1, p2, p3, p4, p5, p6, p7, p8];
+    for p in &mut points {
         p.rotate_x(15.0);
         p.rotate_y(15.0);
-        p.rotate_z(15.0);
-        p.show();
     }
+
+    for angle in 0..=5 {
+        for p in &mut points {
+            spin(p, angle as f64);
+        }
+        thread::sleep(duration);
+    }
+}
+
+fn spin(p: &mut Point, angle: f64) {
+    p.rotate_z(angle);
+    p.show();
 }
