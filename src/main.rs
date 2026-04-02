260fac5253bfb2739efc89288bfff6cf868dd863
Jan <he.klocuszek123@gmail.com>
2026-04-02 19:18:29 +0200

    Fully working rotation

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,34 @@
-use std::{ thread, time };
-
-struct Point {
-    x: f64,
-    y: f64,
-    z: f64,
-}
-
-impl Point {
-    fn show(&self) {
-        println!("({}, {}, {})", self.x, self.y, self.z);
-    }
-
-    fn rotate_x (&mut self, angle: f64) {
-        let radians: f64 = angle.to_radians();
-        let y: f64 = self.y * radians.cos() - self.z * radians.sin();
-        let z: f64 = self.y * radians.sin() + self.z * radians.cos();
-        self.y = y;
-        self.z = z;
-    }
-
-    fn rotate_y (&mut self, angle: f64) {
-        let radians: f64 = angle.to_radians();
-        let x: f64 = self.x * radians.cos() + self.z * radians.sin();
-        let z: f64 = -1.0 * self.x * radians.sin() + self.z * radians.cos();
-        self.x = x;
-        self.z = z;
-    }
-
-    fn rotate_z (&mut self, angle: f64) {
-        let radians: f64 = angle.to_radians();
-        let x: f64 = self.x * radians.cos() - self.y * radians.sin();
-        let y: f64 = self.x * radians.sin() + self.y * radians.cos();
-        self.x = x;
-        self.y = y;
-    }
-
-    // This just spins one point at the time
-    // --------------------------------------
-    // fn spin_z (&mut self) {
-    //     let duration: time::Duration = time::Duration::from_millis(500);
-    //     loop {
-    //         for i in 1..=365 {
-    //             self.rotate_z(i as f64);
-    //             self.show();
-    //             thread::sleep(duration);
-    //         }
-    //     }
-    // }
-}
+mod rotation;
 
 fn main() {
-    let duration: time::Duration = time::Duration::from_millis(500);
-    let p1: Point = Point {
-        x: 0.5,
-        y: 0.7,
-        z: 0.5,
-    };
-    let p2: Point = Point {
-        x: 0.5,
-        y: -0.7,
-        z: 0.5,
-    };
-    let p3: Point = Point {
-        x: 0.5,
-        y: 0.7,
-        z: -0.5,
-    };
-    let p4: Point = Point {
-        x: 0.5,
-        y: -0.7,
-        z: -0.5,
-    };
-    let p5: Point = Point {
-        x: -0.5,
-        y: 0.7,
-        z: 0.5,
-    };
-    let p6: Point = Point {
-        x: -0.5,
-        y: -0.7,
-        z: 0.5,
-    };
-    let p7: Point = Point {
-        x: -0.5,
-        y: 0.7,
-        z: -0.5,
-    };
-    let p8: Point = Point {
-        x: -0.5,
-        y: -0.7,
-        z: -0.5,
-    };
+    let mut points: Vec<rotation::Point> = Vec::new();
+    let xs = [-0.5, 0.5];
+    let ys = [-0.5, 0.5];
+    let zs = [-0.5, 0.5];
 
-    let mut points: [Point; 8] = [p1, p2, p3, p4, p5, p6, p7, p8];
+    // Create cube (8 points in space)
+    for &x in &xs {
+        for &y in &ys {
+            for &z in &zs {
+                points.push(rotation::Point{ x, y, z });
+            }
+        }
+    }
+
+    // Initial tilt
     for p in &mut points {
-        p.rotate_x(15.0);
-        p.rotate_y(15.0);
+        p.rotate_rpy(45.0, 25.0, 15.0);
+        p.show();
     }
 
     for angle in 0..=5 {
         for p in &mut points {
             spin(p, angle as f64);
         }
-        thread::sleep(duration);
     }
 }
 
-fn spin(p: &mut Point, angle: f64) {
+fn spin(p: &mut rotation::Point, angle: f64) {
     p.rotate_z(angle);
     p.show();
 }
---
+++ src/rotation.rs
@@ -0,0 +1,50 @@
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Point {
+    pub fn show(&self) {
+        println!("({}, {}, {})", self.x, self.y, self.z);
+    }
+
+    pub fn rotate_x (&mut self, angle: f64) -> &mut Self {
+        let radians: f64 = angle.to_radians();
+        let y: f64 = self.y * radians.cos() - self.z * radians.sin();
+        let z: f64 = self.y * radians.sin() + self.z * radians.cos();
+
+        self.y = y;
+        self.z = z;
+
+        self
+    }
+
+    pub fn rotate_y (&mut self, angle: f64) -> &mut Self {
+        let radians: f64 = angle.to_radians();
+        let x: f64 = self.x * radians.cos() + self.z * radians.sin();
+        let z: f64 = -1.0 * self.x * radians.sin() + self.z * radians.cos();
+
+        self.x = x;
+        self.z = z;
+
+        self
+    }
+
+    pub fn rotate_z (&mut self, angle: f64) -> &mut Self {
+        let radians: f64 = angle.to_radians();
+        let x: f64 = self.x * radians.cos() - self.y * radians.sin();
+        let y: f64 = self.x * radians.sin() + self.y * radians.cos();
+
+        self.x = x;
+        self.y = y;
+
+        self
+    }
+
+    pub fn rotate_rpy (&mut self, alpha: f64, beta: f64, gamma: f64) -> &mut Self {
+        self.rotate_x(gamma)
+            .rotate_y(beta)
+            .rotate_z(alpha)
+    }
+}
